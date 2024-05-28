6918b544ee4e5b1250db737bc8fc4b541ba1ac2f
Becky Van Vickle <opticalchick@yahoo.com>
2024-05-28 15:14:01 -0500

    added code

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,30 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+	private static final String HOST = "localhost";
+	private static final String PASSWORD = "projects";
+	private static final int PORT = 3306;
+	private static final String SCHEMA = "projects";
+	private static final String USER = "projects";
+
+	
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA,
+				USER, PASSWORD);
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Connection successfully obtained");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("Error Connecting to "+url);
+			throw new DbException(e);
+		}		
+	}	
+}
